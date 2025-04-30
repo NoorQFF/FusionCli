200e01f1919ece7ec43e9f466bc451e468d6e1df
Noor.qazi-at-855930651106 <noor.qazi@fusionfamily.com>
2025-04-30 11:52:47 -0600

    changed default pr destination to last branch rather tahtn always dev

--- utils/git_utils.py
+++ utils/git_utils.py
@@ -2,13 +2,11 @@ import os
 import subprocess
 from utils._utils import runSystemFittedProcess, throwError
 
-def getBasePathName():
-    cwd = os.getcwd()  # Get the current working directory
-    dir_name = os.path.basename(cwd)  # Extract the name of the directory
-    return dir_name
-
 def getCurrentGitBranchName():
     return runSystemFittedProcess('git rev-parse --abbrev-ref HEAD')
+
+def getLastBranchedOffBranchName():
+    return runSystemFittedProcess('git reflog | grep "checkout: moving from" | grep -m1 "$(git rev-parse --abbrev-ref HEAD)" | sed -E \'s/.*moving from ([^ ]+) to.*/\1/\'')
 
 def getLatestCommitMessage() -> str:
     try:
